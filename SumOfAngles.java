4c44abb4222630317e333ebb34400a9c28f43eb7
Sophia Okito <63434504+Sophia-Okito@users.noreply.github.com>
2021-04-23 20:27:02 +0100

    Add files via upload

---
+++ SumOfAngles.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class SumOfAngles {
+
+    public static void main(String[] args) {
+        System.out.println(sumOfAngles(3));
+    }
+    public static int sumOfAngles(int n) {
+        return (n - 2)*180;
+    }
+}
