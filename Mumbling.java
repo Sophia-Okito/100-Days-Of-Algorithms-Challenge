56b10f064a7910e4d164cccdbab826faac52e3ef
Sophia Okito <63434504+Sophia-Okito@users.noreply.github.com>
2021-04-28 23:34:52 +0100

    Update Mumbling.java

--- Mumbling.java
+++ Mumbling.java
@@ -9,11 +9,8 @@ public class Mumbling {
         String[] arr = s.toLowerCase().split("");
         StringBuilder text = new StringBuilder();
         for (int i = 1; i <= s.length(); i++) {
-            int j = 0;
-            do {
-                text.append(j == 0 ? arr[i - 1].toUpperCase() : arr[i - 1]);
-                j++;
-            } while (j < i);
+            text.append(arr[i-1].toUpperCase());
+            text.append(arr[i-1].repeat(i - 1));
             text.append(i == s.length() ? "" : "-");
         }
        return text.toString();
