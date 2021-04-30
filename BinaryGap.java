b373341afaf3a48b987b1eeaf704d82ed3998469
Sophia Okito <63434504+Sophia-Okito@users.noreply.github.com>
2021-04-30 22:24:50 +0100

    Add files via upload

---
+++ BinaryGap.java
@@ -0,0 +1,24 @@
+package com.company;
+
+class BinaryGap {
+    public static void main(String[] args) {
+        System.out.println(solution(1041));
+    }
+    public static int solution(int x) {
+        String binary = Integer.toBinaryString(x);
+        int count = 0;
+        int maxCount = 0;
+
+        for (int i = 0; i < binary.length(); i++) {
+            if (binary.charAt(i) == '0') {
+                count++;
+            } else {
+                if(count > maxCount) {
+                    maxCount = count;
+                    count = 0;
+                }
+            }
+        }
+        return maxCount;
+    }
+}
